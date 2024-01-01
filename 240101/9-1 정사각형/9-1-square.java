aa3107cdca5237614a7856812242c7791bdc4e2e
SongSeungJu <71385868+s0613@users.noreply.github.com>
2024-01-01 22:12:47 +0900

    Title: 9-1 정사각형, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240101/9-1 정사각형/9-1-square.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 10;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                cnt--;
+                System.out.print(cnt);
+                if(cnt == 1) cnt = 10;
+            }
+            System.out.println();
+        }
+    }
+}
