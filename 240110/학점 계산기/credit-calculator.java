ba595635f6cb10b6fb48fa342fa522a0021433b7
SongSeungJu <71385868+s0613@users.noreply.github.com>
2024-01-10 12:44:24 +0900

    Title: 학점 계산기, Time: 162ms, Memory: 12MB, Status: Passed - CodeTree

--- 240110/학점 계산기/credit-calculator.java
+++ 240110/학점 계산기/credit-calculator.java
@@ -10,9 +10,9 @@ public class Main {
             sum += score;
         }
         System.out.printf("%.1f \n",sum/n);
-        if(sum/n > 4.0){
+        if(sum/n >= 4.0){
             System.out.print("Perfect");
-        }else if(sum/n > 3.0){
+        }else if(sum/n >= 3.0){
             System.out.print("Good");
         }
         else System.out.print("Poor");
