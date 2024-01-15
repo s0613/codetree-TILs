bd60adb33a8918e769f4632912156e7fe975a642
SongSeungJu <71385868+s0613@users.noreply.github.com>
2024-01-15 17:26:17 +0900

    Title: 연속부분수열일까, Time: 148ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240115/연속부분수열일까/contiguous-array-or-not.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int[] arr1 = new int[n1];
+        int[] arr2 = new int[n2];
+
+        for(int i = 0; i < n1; i++){
+            arr1[i] = sc.nextInt();
+                    }
+        for(int i = 0; i < n2; i++){
+            arr2[i] = sc.nextInt();
+        }
+
+        int cnt = 0;
+        int cnt2 = 0;
+        boolean damn = true;
+        while(damn){
+            if(arr1[cnt] == arr2[cnt2]){
+                if(n1 > n2){
+                    int cnt3 = 0;
+                    for(int i = 0; i < n2; i++){
+                        if(arr1[cnt+i] == arr2[i]){
+                            cnt3++;
+                        }
+                    }
+                    if(cnt3 == n2){
+                        System.out.print("Yes");
+                        damn = false;
+                        break;
+                    }
+                    else{
+                        System.out.print("No");
+                        break;
+                    }
+                }
+                else{
+                    int cnt3 = 0;
+                    for(int i = 0; i < n2; i++){
+                        if(arr1[i] == arr2[cnt2+i]){
+                            cnt3++;
+                        }
+                    }
+                    if(cnt3 == n1){
+                        System.out.print("Yes");
+                        damn = false;
+                        break;
+                    }
+                    else {
+                        System.out.print("No");
+                        break;
+                    }
+                }
+            }else{
+                if(n1 > n2){
+                     cnt++;
+                }else cnt2++;
+            }
+
+            if(cnt == n1 || cnt2 == n2){
+                System.out.print("No");
+                break;
+            }
+        }
+    }
+}
