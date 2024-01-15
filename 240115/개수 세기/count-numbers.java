575f5558725eab8d67cac3bdaa5b201e79f881b4
SongSeungJu <71385868+s0613@users.noreply.github.com>
2024-01-15 16:52:20 +0900

    Title: 개수 세기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240115/개수 세기/count-numbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            int temp = sc.nextInt();
+            if(temp == m){
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
