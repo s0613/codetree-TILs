68c6b2c0ca01b04f5fd50dd42c42089afafcd78d
SongSeungJu <71385868+s0613@users.noreply.github.com>
2024-01-18 14:35:21 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240118/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int max = Integer.MIN_VALUE;
+        int num = 0;
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            int temp = sc.nextInt();
+            arr[i] = temp;
+            if(max < temp){
+                max = temp;
+                num = i;
+            }
+        }
+        System.out.print(num+1 + " ");
+        int num2 = n;
+        for(int i = 0; i < num2; i++){
+            max = 0;
+            for(int j = 0; j < num; j++){
+                if(max < arr[j]){
+                    max = arr[j];
+                    num2 = j;
+                }
+            }
+            System.out.print(num2+1 + " ");
+            num = num2;
+        }
+    }
+}
