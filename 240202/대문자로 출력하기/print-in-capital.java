2158bb7b1a181b4e5b034d867ce3e5865b622a10
SongSeungJu <71385868+s0613@users.noreply.github.com>
2024-02-02 17:37:53 +0900

    Title: 대문자로 출력하기, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240202/대문자로 출력하기/print-in-capital.java
+++ 240202/대문자로 출력하기/print-in-capital.java
@@ -1,18 +1,24 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        char[] arr = str.toCharArray();
-        for(int i = 0; i < arr.length; i++){
-            if((int)arr[i] >= 65 && (int)arr[i] <= 122){
-                if((int)arr[i] >= 97){
-                    System.out.print((char)((int)arr[i]-32));
-                }else{
-                    System.out.print(arr[i]);
-                }
-            }
-        }
+
+        // 문자열을 정의합니다.
+        String str;
         
+        // 문자열을 입력받습니다.
+        str = sc.next();
+        
+        // 문자열의 길이를 구합니다.
+        int len = str.length();
+        
+        // 문자를 하나하나 확인하여 알파벳일 경우 모두 대문자로 출력합니다.
+        for(int i = 0; i < len; i++) {
+            if(str.charAt(i) >= 'A' && str.charAt(i) <= 'Z')
+                System.out.print(str.charAt(i));
+            else if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z')
+                System.out.print((char)(str.charAt(i) - 'a' + 'A'));
+        }
     }
 }
