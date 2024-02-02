f543d34053d9d55681d7c905cb08a18db61f773b
SongSeungJu <71385868+s0613@users.noreply.github.com>
2024-02-02 17:52:12 +0900

    Title: 소문자와 숫자, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240202/소문자와 숫자/letter-and-number.java
+++ 240202/소문자와 숫자/letter-and-number.java
@@ -6,7 +6,7 @@ public class Main {
         for(int i = 0; i < str.length(); i++){
             if(str.charAt(i) >= 'A' && str.charAt(i) <= 'Z'){
                 System.out.print((char)((int)str.charAt(i)+32));
-            }else if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z' || str.charAt(i) >= '0' && str.charAt(i) < '9'){
+            }else if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z' || str.charAt(i) >= '0' && str.charAt(i) <= '9'){
                 System.out.print(str.charAt(i));
             }
         }
