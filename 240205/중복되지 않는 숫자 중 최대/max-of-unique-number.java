b6acdcdd092b84e5d4451c6f82acc77bc39aa3fe
SongSeungJu <71385868+s0613@users.noreply.github.com>
2024-02-05 16:19:05 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 237ms, Memory: 13MB, Status: Passed - CodeTree

--- 240205/중복되지 않는 숫자 중 최대/max-of-unique-number.java
+++ 240205/중복되지 않는 숫자 중 최대/max-of-unique-number.java
@@ -1,37 +1,40 @@
-import java.util.*;
-import java.lang.*;
+import java.util.Scanner;
+
 public class Main {
+    public static final int MAX_N = 1000;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int max = Integer.MIN_VALUE;
-        int num = 0;
 
-        int[] arr = new int[n];
+        int n;
+        int[] nums = new int[MAX_N];
 
-        for(int i = 0; i < n; i++){
-            int temp = sc.nextInt();
-            arr[i] = temp;
-        }
+        // 입력
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            nums[i] = sc.nextInt();
 
-        for(int i = 0; i < arr.length; i++){
-            if(max < arr[i]){
-                max = arr[i];
-            }else if(max == arr[i]){
-                num++;
-                if(num == n/2){
-                    max = -1;
-                    break;
-                }
-                int idx = 0;
-                for(int j : arr){
-                    if(j != arr[i]){
-                        arr[idx++] = j;
+        // 최댓값 찾기
+        int max = -1;
+        for(int i = 0; i < n; i++) {
+            int currNum = nums[i];
+            // 최대가 될 수 있는 후보입니다.
+            if(max < currNum) {
+                // 갱신할 수 있는지 확인하기 위해 이 숫자의 등장 빈도를 셉니다.
+                int count = 0;
+                for(int j = 0; j < n; j++) {
+                    if(nums[j] == currNum) {
+                        count++;
                     }
                 }
-                i = 0;
+                // 이 숫자가 배열에서 유일할때만 갱신합니다.
+                if(count == 1) {
+                    max = currNum;
+                }
             }
         }
+
+        // 최댓값과 최솟값을 출력합니다.
         System.out.print(max);
     }
 }
