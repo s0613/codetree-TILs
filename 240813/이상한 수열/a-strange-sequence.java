a2fc5c64717d382e22ddbec1124489f11c1762c5
SongSeungJu <71385868+s0613@users.noreply.github.com>
2024-08-13 12:55:37 +0900

    Title: 이상한 수열, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240813/이상한 수열/a-strange-sequence.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = infact(n);
+        System.out.print(sum);
+    }
+    public static int infact(int n){
+        if(n == 1){
+            return 1;
+        }
+        if(n == 2){
+            return 2;
+        }
+        return infact(n/3) + infact(n-1);
+    }
+}
