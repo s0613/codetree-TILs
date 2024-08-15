9d7328a970d9a7252a8e5a0e280569965023e15e
SongSeungJu <71385868+s0613@users.noreply.github.com>
2024-08-15 11:40:35 +0900

    Title: Top K 숫자 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240815/Top K 숫자 구하기/kth-number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i ++){
+            arr[i] = sc.nextInt();
+        }
+        Integer[] it = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        Arrays.sort(it, Collections.reverseOrder());
+        System.out.print(it[k]);
+    }
+}
