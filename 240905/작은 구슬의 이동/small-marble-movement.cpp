f8bace120ce3442f9f34494a2586a11e79a03d93
SongSeungJu <71385868+s0613@users.noreply.github.com>
2024-09-05 17:43:05 +0900

    Title: 작은 구슬의 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240905/작은 구슬의 이동/small-marble-movement.cpp
+++ 240905/작은 구슬의 이동/small-marble-movement.cpp
@@ -24,22 +24,25 @@ int main() {
     else dir = 1;
 
     for(int i = 0; i < t; i++){
-    
+        
         if(r+dx[dir] >= 1 && c+dy[dir] >= 1 && r+dx[dir] <= n && c+dy[dir] <= n){
             r += dx[dir];
             c += dy[dir];
         }
         else{
             if(d == 'U'){
+                d = 'D';
         dir = 2;
     }
     else if(d == 'D'){
+        d = 'U';
         dir = 0;
     }
     else if(d == 'R'){
+        d = 'L';
         dir = 1;
     }
-    else dir = 3;
+    else {dir = 3; d = 'R';}
         }
     }
     cout << r << " " << c;
