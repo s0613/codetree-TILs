f2f6bfd1a071f83097e78e34c062f188b35529d8
SongSeungJu <71385868+s0613@users.noreply.github.com>
2024-09-06 20:31:03 +0900

    Title: 되돌아오기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240906/되돌아오기/come-back.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int t = 0;
+    int n;
+    cin >> n;
+    int dx[4] = {-1,0,1,0};
+    int dy[4] = {0,1,0,-1};
+    int dir = 0;
+    int x = 0, y =0;
+    int cnt = 0;
+    bool test = true;
+
+    while(n > 0){
+        char cmd;
+        int num;
+        cin >> cmd >> num;
+        if(cmd == 'N'){
+            dir = 0;
+        }
+        else if(cmd == 'E'){
+            dir = 1;
+        }
+        else if(cmd == 'S'){
+            dir = 2;
+        }
+        else dir = 3;
+
+        for(int i = 0; i < num; i++){
+            cnt++;
+            x += dx[dir]; y += dy[dir];
+            if(x == 0 && y == 0){
+                cout << cnt;
+                test = false;
+                break;
+            }
+        }
+        
+        n--;
+    }
+    if(test){
+        cout << "-1";
+    }
+    return 0;
+}
