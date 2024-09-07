cb3494cd1ba2787f852cf8cfaa1e86bf732c47ee
SongSeungJu <71385868+s0613@users.noreply.github.com>
2024-09-08 08:56:16 +0900

    Title: 되돌아오기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240908/되돌아오기 2/come-back-2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int dx[4] = {0,1,0,-1};
+    int dy[4] = {1,0,-1,0};
+    int dir = 0;
+    int x = 0, y = 0;
+    string cmd;
+    cin >> cmd;
+    int cnt = 0;
+
+    for(int i = 0; i < cmd.length(); i++){
+        if(cmd[i] == 'F'){
+            x += dx[dir];
+            y += dy[dir];
+        }
+        else if(cmd[i] == 'R'){
+            dir += 1;
+            dir %= 4;
+        }
+        else {
+            dir -= 1;
+            if(dir < 0){
+                dir = 3;
+            }
+        }
+
+        cnt++;
+        if(x == 0 && y == 0){
+            cout << cnt;
+            break;
+        }
+    }
+    return 0;
+}
