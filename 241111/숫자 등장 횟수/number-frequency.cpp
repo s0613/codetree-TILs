3f55c496556bc8a54adec9bd41faebd9f12f7dfa
SongSeungJu <71385868+s0613@users.noreply.github.com>
2024-11-11 08:53:02 +0900

    Title: 숫자 등장 횟수, Time: 99ms, Memory: 1MB, Status: Passed - CodeTree

--- 241111/숫자 등장 횟수/number-frequency.cpp
+++ 241111/숫자 등장 횟수/number-frequency.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
-#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 int main() {
-    
     int n, m;
     cin >> n >> m;
-    vector<int> arr(n,0);
-    vector<int> numarr(n+1,0);
-    for(int i = 0; i < n; i++){
-        cin >> arr[i];
-        numarr[arr[i]]++;
+    
+    unordered_map<int, int> numarr;  // 숫자의 빈도수를 저장하는 맵
+    for (int i = 0; i < n; i++) {
+        int val;
+        cin >> val;
+        numarr[val]++;  // 입력된 값을 카운트
     }
     
-    while(m--){
+    while (m--) {
         int a;
         cin >> a;
-        cout << numarr[a] << " ";
+        cout << numarr[a] << " ";  // 존재하지 않으면 기본값 0을 출력
     }
-    
-        
     
     return 0;
 }
