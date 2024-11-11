e5f9084e7821074170eda82d09a67bb1c3d0ac76
SongSeungJu <71385868+s0613@users.noreply.github.com>
2024-11-11 18:40:13 +0900

    Title: 두 수의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241111/두 수의 합/sum-of-two-num.cpp
+++ 241111/두 수의 합/sum-of-two-num.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 using namespace std;
 
 int main() {
     int n, m;
     cin >> n >> m;
-    vector<int> arr(n);
+    unordered_map<int,int> um;
     for(int i = 0; i < n; i++){
-        cin >> arr[i];
+        int temp;
+        cin >> temp;
+        um[temp]++;
     }
-    int num = 0;
-    for(int i = 0; i < n; i++){
-        for(int j = i+1; j < n; j++){
-            if(arr[i] + arr[j] == m){
-                num++;
-            }
+    int count = 0;
+    for(auto i = um.begin(); i != um.end(); i++){
+        if(um.find(m-(i->first)) != um.end()){
+            count += um[m-(i->first)];
         }
     }
-    cout << num;
+    cout << count/2;
     return 0;
 }
